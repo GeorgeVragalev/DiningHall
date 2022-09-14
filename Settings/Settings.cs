9b1c40417676313a0c962916389975e9fce08346
George Vragalev <78079918+GeorgeVragalev@users.noreply.github.com>
2022-09-14 07:52:47 +0300

    url for local setup

--- Settings/Settings.cs
+++ Settings/Settings.cs
@@ -4,7 +4,7 @@ public static class Settings
 {
     public static readonly int Tables = 10;
     public static readonly int Waiters = 5;
-    public static readonly string KitchenUrl = "http://localhost:5001/order"; //docker
-    // public static readonly string KitchenUrl = "https://localhost:7091/order"; //local
+    // public static readonly string KitchenUrl = "http://localhost:5001/order"; //docker
+    public static readonly string KitchenUrl = "https://localhost:7091/order"; //local
     public static readonly string TimeUnit= "Seconds";
 }
