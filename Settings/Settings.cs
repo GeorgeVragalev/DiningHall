7be74f0f5d94ec8b5cd4c0ab162e9f5760cfd7a1
George Vragalev <78079918+GeorgeVragalev@users.noreply.github.com>
2022-09-16 10:08:13 +0300

    docker configured

--- Settings/Settings.cs
+++ Settings/Settings.cs
@@ -4,7 +4,15 @@ public static class Settings
 {
     public static readonly int Tables = 10;
     public static readonly int Waiters = 5;
-    // public static readonly string KitchenUrl = "http://localhost:5001/order"; //docker
-    public static readonly string KitchenUrl = "https://localhost:7091/order"; //local
+    public static readonly string KitchenUrl = "http://host.docker.internal:7091/order"; //docker
+    // public static readonly string KitchenUrl = "https://localhost:7091/order"; //local
     public static readonly string TimeUnit= "Seconds";
 }
+/*
+to run docker for dininghall container: 
+BUILD IMAGE:
+docker build -t dininghall .
+
+RUN CONTAINER: map local_port:exposed_port
+docker run --name dininghall-container -p 7090:80 dininghall
+*/
